c7161232bf2a4b1514963cde6e70c163985561b1
danhorea2010 <danhorea2010@gmail.com>
2022-11-21 17:40:23 +0200

    fixed macro schema being string instead of int

--- db/migrate/20221121153552_create_macros.rb
+++ db/migrate/20221121153552_create_macros.rb
@@ -3,8 +3,7 @@ class CreateMacros < ActiveRecord::Migration[7.0]
     create_table :macros do |t|
       t.integer :protein
       t.integer :fat
-      t.string :carbs
-      t.string :integer
+      t.integer :carbs
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_152427) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_153552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,8 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_152427) do
   create_table "macros", force: :cascade do |t|
     t.integer "protein"
     t.integer "fat"
-    t.string "carbs"
-    t.string "integer"
+    t.integer "carbs"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
